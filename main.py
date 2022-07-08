2412f751a8c67d68d934bcf74c5308f780f8a56e
Artur Yarashev <artur.iarashiev.228@mail.ru>
2022-07-08 20:04:12 +0600

    change some numbers

--- main.py
+++ main.py
@@ -4,29 +4,13 @@ import numpy
 photo = numpy.zeros((450, 450, 3), dtype='uint8')
 
 photo[:] = 55, 220, 31
-photo[100:150, 200:280] = 255, 0, 130
+photo[100:150, 200:280] = 165, 0, 230
 
-cv2.rectangle(photo, (20, 20), (100, 100), (255, 0, 130), thickness=3)
+cv2.rectangle(photo, (20, 20), (150, 100), (255, 0, 130), thickness=3)
 cv2.line(photo, (0, photo.shape[0] // 2), (photo.shape[1], photo.shape[0] // 2), (255, 255, 255), thickness=2)
 
 cv2.imshow('Photo', photo)
 cv2.waitKey(0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
