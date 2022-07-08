6c6bd679e7f0c5b6a07a4d8299fc143f9a7ab7ca
Artur Yarashev <artur.iarashiev.228@mail.ru>
2022-07-08 15:42:17 +0600

    first commit test CV2

---
+++ main.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy
+
+photo = numpy.zeros((450, 450, 3), dtype='uint8')
+
+photo[:] = 55, 220, 31
+photo[100:150, 200:280] = 255, 0, 130
+
+cv2.rectangle(photo, (20, 20), (100, 100), (255, 0, 130), thickness=3)
+cv2.line(photo, (0, photo.shape[0] // 2), (photo.shape[1], photo.shape[0] // 2), (255, 255, 255), thickness=2)
+
+cv2.imshow('Photo', photo)
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# x = 1
+#
+# img = cv2.imread('images/img.jpg')
+# img = cv2.resize(img, (img.shape[1] // x, img.shape[0] // x))
+# img = cv2.GaussianBlur(img, (5, 3), 4)
+# img = cv2.Canny(img, 150, 200)
+#
+# kernel = numpy.ones((5, 5), numpy.uint8)
+# img = cv2.dilate(img, kernel, iterations=1)
+# img = cv2.erode(img, kernel, iterations=1)
+#
+# cv2.imshow('Result', img)
+#
+# cv2.waitKey(0)
