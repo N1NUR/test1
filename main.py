5062eba211eb5f3b7f01ec7afd48aa7ee0f19cec
Artur Yarashev <artur.iarashiev.228@mail.ru>
2022-07-09 02:03:36 +0600

    add print functions and change color

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy
 
+print('some string')
+print('Close this window for continue')
 photo = numpy.zeros((450, 450, 3), dtype='uint8')
 
-photo[:] = 55, 220, 31
+photo[:] = 165, 150, 73
 photo[100:150, 200:280] = 165, 0, 230
 
 cv2.rectangle(photo, (20, 20), (150, 100), (255, 0, 130), thickness=3)
@@ -11,6 +13,7 @@ cv2.line(photo, (0, photo.shape[0] // 2), (photo.shape[1], photo.shape[0] // 2),
 
 cv2.imshow('Photo', photo)
 cv2.waitKey(0)
+
 
 
 
